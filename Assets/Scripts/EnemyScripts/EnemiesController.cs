3e9f34262e259fbc6e62afecef8a2e185bb24ff1
LePon <edoardopoma98@gmail.com>
2023-06-08 18:21:04 +0200

    EnemyAttack

--- Assets/Scripts/EnemyScripts/EnemiesController.cs
+++ Assets/Scripts/EnemyScripts/EnemiesController.cs
@@ -11,26 +11,39 @@ public class EnemiesController : MonoBehaviour
     [Header("Patrolling")]
     [SerializeField] GameObject[] patrolPoints;
     [SerializeField] int maxRange;
+    //[SerializeField] float patrolTimer;
+    //float timer;
     int minRange = 0;
+
 
     [Header("Attack")]
     [SerializeField] float playerDistance;
     [SerializeField] float timeBeforeAttack;
     [SerializeField] float enemyAttackSpeed;
+    [SerializeField] float knockBackForce;
     float attackTimer;
 
     CarController carController;
+    //Rigidbody rb;
+
+    Vector3 firstPosition;
+    Quaternion firstRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         carController = FindObjectOfType<CarController>();
+        //rb = GetComponent<Rigidbody>();
+
+        firstPosition = transform.position;
+        firstRotation = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        EnemyMovement();
+        if(!isStunned)
+            EnemyAttack();
     }
 
     int index;
@@ -47,23 +60,56 @@ public class EnemiesController : MonoBehaviour
         }
     }
 
-    //void EnemyAttack()
+    void EnemyAttack()
+    {
+        float distance = Vector3.Distance(carController.transform.position, transform.position);
+        var lookPosition = carController.transform.position - transform.position;
+        lookPosition.y = 0;
+        var lookRotation = Quaternion.LookRotation(lookPosition);
+
+        if (playerDistance >= distance)
+        {
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, enemyRotationSpeed * Time.deltaTime);
+            Vector3 enemyPos = new Vector3(carController.transform.position.x, 0, carController.transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, enemyPos, enemyAttackSpeed * Time.deltaTime);
+
+        }
+        else
+        {
+            EnemyMovement();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            isStunRunning = true;
+            StartCoroutine(EnemyStun());
+            //KnockBack();
+        }
+    }
+
+    private bool isStunned;
+    private bool isStunRunning;
+    private IEnumerator EnemyStun()
+    {
+        if (isStunRunning)
+        {
+            isStunned = true;
+            yield return new WaitForSecondsRealtime(3f);
+            isStunned = false;
+            isStunRunning = false;
+        }
+
+    }
+
+    //private void KnockBack()
     //{
-    //    float distance = Vector3.Distance(carController.transform.position, transform.position);
-    //    var lookPosition = carController.transform.position - transform.position;
-    //    lookPosition.y = 0;
-    //    var lookRotation = Quaternion.LookRotation(lookPosition);
-
-    //    if(playerDistance >= distance)
-    //    {
-    //        attackTimer += Time.deltaTime;
-
-    //        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, enemyRotationSpeed * Time.deltaTime);
-
-    //        if(attackTimer >= timeBeforeAttack)
-    //        {
-    //            Vector3 enemyPos = Vector3.MoveTowards(transform.position, enemyPos, ene)
-    //        }
-    //    }
+    //    Vector3 targetPos = carController.gameObject.transform.position;
+    //    Vector3 startPos = transform.position;
+    //    rb.AddForce((startPos - targetPos) * knockBackForce, ForceMode.Impulse);
+    //    //rb.AddForce((targetPos - startPos) * knockBackForce, ForceMode.Impulse);
     //}
 }
