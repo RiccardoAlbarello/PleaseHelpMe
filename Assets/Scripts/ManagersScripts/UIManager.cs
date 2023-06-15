857fc0dc81efa20c35943692acad933da39c3533
LePon <edoardopoma98@gmail.com>
2023-06-15 14:53:01 +0200

    StartMenu_UIManager

---
+++ Assets/Scripts/ManagersScripts/UIManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject optionsCanvas;
+
+    public void PlayGame()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Settings()
+    {
+        optionsCanvas.SetActive(true);
+    }
+
+    public void QuitSettings()
+    {
+        optionsCanvas.SetActive(false);
+    }
+}
