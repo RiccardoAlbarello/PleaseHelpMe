29c0eea47f986a0bacf2105f4bc3a7ee731a5a8c
LePon <edoardopoma98@gmail.com>
2023-06-29 12:05:05 +0200

    Menu

--- Assets/Scripts/ManagersScripts/UIManager.cs
+++ Assets/Scripts/ManagersScripts/UIManager.cs
@@ -6,38 +6,7 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] GameObject optionsCanvas;
-    [SerializeField] GameObject pauseMenu;
-
-    public static bool GameIsPaused = false;
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (GameIsPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
-        }
-    }
-
-    void Resume()
-    {
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPaused = false;
-    }
-
-    void Pause()
-    {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0f;
-        GameIsPaused = true;
-    }
+    
 
     public void PlayGame()
     {
